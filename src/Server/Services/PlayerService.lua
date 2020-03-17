4d808f130c558752deeab352af263c625c4adfd6
Brandon Wilcox <itsmrasync@gmail.com>
2020-03-16 22:30:04 -0400

    Create PlayerService.lua

---
+++ src/Server/Services/PlayerService.lua
@@ -0,0 +1,20 @@
+-- Player Service
+-- Username
+-- March 16, 2020
+
+
+
+local PlayerService = {Client = {}}
+
+
+function PlayerService:Start()
+	
+end
+
+
+function PlayerService:Init()
+	
+end
+
+
+return PlayerService
