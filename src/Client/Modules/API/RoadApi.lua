50add1a4461cb43fcb5a23470e4dc69d0562f17e
Brandon Wilcox <stixxhihat@gmail.com>
2020-04-05 23:09:46 -0400

    Update RoadApi.lua

--- src/Client/Modules/API/RoadApi.lua
+++ src/Client/Modules/API/RoadApi.lua
@@ -33,10 +33,12 @@ local VehicleClass
 
 --//Locals
 local PlotObject
+local RandomObject
 
 
 --//Returns an array containing all adjacent roads
-function RoadApi:GetAdjacentRoads(currentRoad)
+--//Region3 based
+local function GetAdjacentRoads(currentRoad, lastRoad)
     local roadPosition = currentRoad.PrimaryPart.Position
     local adjacentRegion = Region3.new(roadPosition - Vector3.new(1, 1, 1), roadPosition + Vector3.new(1, 1, 1))
     local roadsInRegion = workspace:FindPartsInRegion3WithWhiteList(adjacentRegion, PlotObject.Placements.Roads:GetChildren(), math.huge)
@@ -48,8 +50,8 @@ function RoadApi:GetAdjacentRoads(currentRoad)
         local model = part:FindFirstAncestorOfClass("Model")
         local position = model.PrimaryPart.Position
         
-        --Only add model if model is not currentRoad, model is not already in index, and if it is directly adjacent
-        if ((model ~= currentRoad) and (not table.find(modelsInRegion, model)) and (roadPosition.X == position.X or roadPosition.Z == position.Z)) then
+        --Only add model if model is not currentRoad model is not already in index, and if it is directly adjacent
+        if ((model ~= currentRoad) and (model ~= lastRoad) and (not table.find(modelsInRegion, model)) and (roadPosition.X == position.X or roadPosition.Z == position.Z)) then
             table.insert(modelsInRegion, model)
         end
     end
@@ -58,13 +60,41 @@ function RoadApi:GetAdjacentRoads(currentRoad)
 end
 
 
+--//Returns a random road from the returned array of adjacent roads
+--//Returns nil of no roads are found
+local function GetNextRoad(currentRoad, lastRoad)
+    local adjacentRoads = GetAdjacentRoads(currentRoad, lastRoad)
+    print(#adjacentRoads)
+
+    return adjacentRoads[RandomObject:NextInteger(1, #adjacentRoads)]
+end
+
+
+--//Generates a path for a vehicle to follow
+--//Traces roads until roads end
+local function GeneratePath(startingRoad)
+    local currentRoad = GetNextRoad(startingRoad)
+    local roads = {startingRoad, currentRoad}
+
+    repeat
+        currentRoad = GetNextRoad(currentRoad, roads[#roads - 1])
+        table.insert(roads, currentRoad)
+    until (not currentRoad)
+
+    for _, road in pairs(roads) do
+        road.PrimaryPart.Transparency = 0
+        road.PrimaryPart.Color = Color3.fromRGB(0, 255, 0)
+
+        wait()
+    end
+end
+
+
 --//Creates interval to spawn vehicles
 function RoadApi:Start()
-    PlotObject.Placements.Roads.ChildAdded:Connect(function(newChild)
-        while (not newChild.PrimaryPart) do wait() end
+    wait(10)
 
-        print(#self:GetAdjacentRoads(newChild))
-    end)
+    GeneratePath(PlotObject.Placements.Roads:GetChildren()[9])
 end
 
 
@@ -81,6 +111,7 @@ function RoadApi:Init()
 
     --//Locals
     PlotObject = self.Player.PlayerPlot.Value
+    RandomObject = Random.new(os.time())
 
 end
 
